9aee2f176b9802946e1e97f0b60bf0a8f201e5b3
cozyGalvinism <reallifejunkies@googlemail.com>
2021-08-26 17:29:03 +0200

    Fix up template for actual usage

--- src/server.rs
+++ src/server.rs
@@ -1,5 +1,7 @@
 use tonic::{transport::Server, Request, Response, Status};
 use ::log::{debug, error, info};
+use crate::log::setup_log;
+use std::env;
 
 use {{proto_name}}::*;
 
@@ -8,6 +10,10 @@ mod log;
 pub mod {{proto_name}} {
     tonic::include_proto!("{{proto_name}}");
 }
+
+// Implement your proto here
+// https://github.com/hyperium/tonic/blob/master/examples/helloworld-tutorial.md
+// https://github.com/hyperium/tonic/blob/master/examples/routeguide-tutorial.md
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
